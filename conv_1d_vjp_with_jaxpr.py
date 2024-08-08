50f60d32a899d4e1ce1286f58b83d7f0bd7cb680
Felix Koehler <f.koehler@tum.de>
2024-08-08 10:16:34 +0200

    Add advanced options

--- conv_1d_vjp_with_jaxpr.py
+++ conv_1d_vjp_with_jaxpr.py
@@ -20,12 +20,38 @@ with st.sidebar:
     batch_size = st.slider("Batch size", 1, 5, 1)
     in_feature_size = st.slider("Input feature size", 1, 5, 1)
     out_feature_size = st.slider("Output feature size", 1, 5, 1)
+    if st.toggle("Advanced options"):
+        left_padding = st.slider("Left padding", 0, 5, 0)
+        right_padding = st.slider("Right padding", 0, 5, 0)
+        stride = st.slider("Stride", 1, 4, 1)
+        lhs_dilation = st.slider("lhs dilation", 1, 3, 1)
+        rhs_dilation = st.slider("rhs dilation", 1, 3, 1)
+    else:
+        left_padding = 0
+        right_padding = 0
+        stride = 1
+        lhs_dilation = 1
+        rhs_dilation = 1
 
 input_array = jax.random.normal(jax.random.PRNGKey(0), (batch_size, in_feature_size, array_size))
 kernel = jax.random.normal(jax.random.PRNGKey(1), (out_feature_size, in_feature_size, kernel_size))
 
-conv_fun = lambda lhs: jax.lax.conv_general_dilated(lhs, kernel, (1,), ((0, 0),))
-filter_fun = lambda rhs: jax.lax.conv_general_dilated(input_array, rhs, (1,), ((0, 0),))
+conv_fun = lambda lhs: jax.lax.conv_general_dilated(
+    lhs,
+    kernel,
+    window_strides=(stride,),
+    padding=((left_padding, right_padding),),
+    lhs_dilation=(lhs_dilation,),
+    rhs_dilation=(rhs_dilation,),
+)
+filter_fun = lambda rhs: jax.lax.conv_general_dilated(
+    input_array,
+    rhs,
+    window_strides=(stride,),
+    padding=((left_padding, right_padding),),
+    lhs_dilation=(lhs_dilation,),
+    rhs_dilation=(rhs_dilation,),
+)
 
 if what_to_display in ["Input", "Both"]:
     if display_primals:
